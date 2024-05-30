a88fe4d2d6e0d3a501e50111a8e7a21c8da255b8
vin.ah <144428051+vin10ah@users.noreply.github.com>
2024-05-30 14:15:23 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,6 +1,9 @@
+# def solution(n):
+#     a = []
+#     for i in range(n+1):
+#         if i % 2 == 1:
+#             a.append(i)
+#     return a
+
 def solution(n):
-    a = []
-    for i in range(n+1):
-        if i % 2 == 1:
-            a.append(i)
-    return a
+    return [i for i in range(n+1) if i % 2 == 1 ]

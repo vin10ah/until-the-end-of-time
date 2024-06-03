0867de7d827688edd3410265b4a17d50bb1b63d7
vin.ah <144428051+vin10ah@users.noreply.github.com>
2024-06-03 09:53:08 +0900

    [level 1] Title: 폰켓몬, Time: 0.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,14 +1,22 @@
+# def solution(nums):
+#     answer = 0
+#     po_dic = {}
+#     for num in nums:
+#         if num in po_dic:
+#             po_dic[num] += 1
+#         else:
+#             po_dic[num] = 1
+#     po_kind = list(po_dic.keys())
+    
+#     return len(po_kind) if len(po_kind) <= len(nums)//2 else len(nums)//2
+
+    # if len(po_kind) <= len(nums)//2:
+    #     return len(po_kind)
+    # else:
+    #     return len(nums)//2
+
 def solution(nums):
     answer = 0
-    po_dic = {}
-    for num in nums:
-        if num in po_dic:
-            po_dic[num] += 1
-        else:
-            po_dic[num] = 1
-    po_kind = list(po_dic.keys())
+    po_set = set(nums)
     
-    if len(po_kind) <= len(nums)//2:
-        return len(po_kind)
-    else:
-        return len(nums)//2
+    return len(po_set) if len(po_set) <= len(nums)//2 else len(nums)//2

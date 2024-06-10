25d62fc09a985efe9d224c13d650c68f17e5c79f
vin.ah <144428051+vin10ah@users.noreply.github.com>
2024-06-10 12:12:09 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,16 +1,39 @@
-def solution(citations):
-    n = len(citations)
-    answer = 0
-    citations.sort()   
-    for h in range(1, n+1):
-        c_cnt = 0
-        if n < h:
-            continue
-        else:  
-            for c in citations:
-                if c >= h:
-                    c_cnt += 1
-        if c_cnt >= h:
-            answer = h
+# def solution(citations):
+#     n = len(citations)
+#     answer = 0
+#     lst = []
+#     citations.sort()   
+#     for h in range(1, n+1):
+#         c_cnt = 0         
+#         for c in citations:
+#             if c >= h:
+#                 c_cnt += 1
+#         if c_cnt >= h:
+#             answer = h
+#             lst.append(h)
     
-    return answer
+#     return lst
+
+# def solution(citations):
+#     n = len(citations)
+#     citations.sort(reverse=True)
+#     answer = 0
+#     for h in citations:
+#         c_cnt = 0       
+#         for i in range(n):
+#             if citations[i] >= h:
+#                 c_cnt += 1
+#         if c_cnt >= h:
+#             return h
+    #         answer = h
+    # return answer
+    
+def solution(citations):
+    citations.sort(reverse=True)
+    h_index = 0
+    for i, citation in enumerate(citations):
+        if citation >= i + 1:
+            h_index = i + 1
+        else:
+            break
+    return h_index

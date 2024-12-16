24b558e6b32c03e541e9bfd38891c65adfbdcf54
SEOBIN <144428051+vin10ah@users.noreply.github.com>
2024-12-16 21:12:43 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.60 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = 0
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] == arr[j][i]:
+                answer = 1
+                continue
+            elif arr[i][j] != arr[j][i]:
+                answer = 0
+                break
+
+    return answer
